94b5509a3fed1c67810db0bfadeeda3fb42a02db
Sonianv <sonianovacovici@gmail.com>
2023-10-15 13:27:12 +0300

    Step 2

--- src/tutorial.cxx
+++ src/tutorial.cxx
@@ -1,6 +1,6 @@
-#include <cmath>
 #include <iostream>
 #include <string>
+#include "MathFunctions.h"
 #include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
@@ -13,9 +13,13 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  // convert input to double
   const double inputValue = std::stod(argv[1]);
+
+  // TODO 6: Replace sqrt with mathfunctions::sqrt
+
   // calculate square root
-  const double outputValue = sqrt(inputValue);
+  const double outputValue = mathfunctions::sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
